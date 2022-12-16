2e5f3588a2403cf07769e9e98f461ad1b1426ee4
Bahadır Arda <bahadirarda96@gmail.com>
2022-12-16 14:34:24 +0300

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -12,17 +12,30 @@ const server = http.createServer((req, res)=> {
     // res.write('<h1>Hello world</h1>')
     // res.end() 
 
-    fs.readFile('./views/index.html', (err, data)=>{
+    let path = './views/'
+    switch(req.url){
+        case '/':
+            path += 'index.html'
+            break
+        case '/about':
+            path += 'about.html'
+            break
+        default:
+            path += '404.html'
+            break
+    }
+
+    fs.readFile(path , (err, data)=>{
         if(err){
-            console.log(err)
+            console.log(err);
             res.end()
         }
         else{
-            res.write(data)
-            res.end()   
+            // res.write(data) 
+            res.end(data)   
         }
     })
-})
+}) 
 
 server.listen(3000,'localhost',()=>{
     console.log('3000 portu dinleniyor');
